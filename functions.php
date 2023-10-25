77e6660d65c85717cd6237d20fbeae5c2838a4da
RonitKaushal <sparkpleazer@gmail.com>
2023-10-25 19:00:15 +0530

    Navbar pages added!

--- functions.php
+++ functions.php
@@ -5,4 +5,10 @@ function register_custom_menu() {
 }
 add_action('init', 'register_custom_menu');
 
+function custom_theme_menu_location() {
+    register_nav_menu('second_main_menu', 'second_main_menu');
+}
+add_action('after_setup_theme', 'custom_theme_menu_location');
+
+
 ?>
--- index.php
+++ index.php
@@ -13,44 +13,21 @@
         </p>
     </div>
     <div class="menu_container">
-            <ul>
-                <li> <a href="https://www.neptunehotels.com/">Home</a></li>
-                <li> <a href="#" style="font-weight: bold">Beach Resorts</a>
-                    <ul>
-                        <li> <a href="https://www.neptunehotels.com/neptune-pwani-beach-resort-spa/">Neptune Pwani Beach
-                                Resort &amp; Spa - Zanzibar</a></li>
-                        <li> <a href="https://www.neptunehotels.com/neptune-palm-beach-boutique-resort-spa/">Neptune
-                                Palm Beach Boutique Resort &amp; Spa - Diani</a></li>
-                        <li> <a href="https://www.neptunehotels.com/neptune-paradise-beach-resort-spa/">Neptune Paradise
-                                Beach Resort &amp; Spa - Diani</a></li>
-                        <li> <a href="https://www.neptunehotels.com/neptune-village-beach-resort-spa/">Neptune Village
-                                Beach Resort &amp; Spa - Diani</a></li>
-                        <li> <a href="https://www.neptunehotels.com/neptune-beach-resort/">Neptune Beach Resort –
-                                Mombasa</a></li>
-                    </ul>
-                </li>
-                <li> <a href="https://www.neptunehotels.com/safari-collection/" style="font-weight: bold">Luxury Safari
-                        Collection</a>
-                    <ul>
-                        <li> <a href="https://www.neptunehotels.com/neptune-ngorongoro-luxury-lodge/">Neptune Ngorongoro
-                                Luxury Lodge - Ngorongoro</a></li>
-                        <li> <a href="https://www.neptunehotels.com/neptune-mara-rianta-luxury-tented-camp/">Neptune
-                                Mara Rianta Luxury Tented Camp – Masai Mara</a></li>
-                    </ul>
-                </li>
-            </ul>
-        <div class="col-md-6">
-            <ul>
-                <li> <a href="https://www.neptunehotels.com/about/" class="small-text">About</a></li>
-                <li> <a href="https://www.neptunehotels.com/contact/" class="small-text">Contact</a></li>
-                <li> <a href="https://www.neptunehotels.com/cleanliness-safety/" class="small-text">Cleanliness &amp;
-                        Safety</a></li>
-                <li> <a href="https://www.neptunehotels.com/wp-content/uploads/2022/08/NEPTUNE-HOTELS-GROUP-PROFILE.pdf"
-                        class="small-text" target="_blank">Brochures</a></li>
-                <li> <a href="https://www.neptunehotels.com/faqs/" class="small-text">FAQs</a></li>
-                <li> <a href="https://www.neptunehotels.com/blog/" class="small-text">Blog</a></li>
-            </ul>
-        </div>
+    <?php
+    wp_nav_menu(array(
+        'theme_location' => 'primary-menu',
+        'menu_id' => 'primary-menu',
+        'container' => false, // Remove the menu container
+    ));
+    ?>
+    </div>
+    <div class="menu_container">
+    <?php
+wp_nav_menu(array(
+    'theme_location' => 'second_main_menu',
+    'menu_id' => 'second_main_menu',
+));
+    ?>
     </div>
 </nav>
 
