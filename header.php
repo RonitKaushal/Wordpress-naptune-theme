5ac93833419fc567536ce0fbb1d4305d3fa9be8c
RonitKaushal <sparkpleazer@gmail.com>
2023-10-27 17:52:06 +0530

    Nevigation menu made

--- header.php
+++ header.php
@@ -5,5 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Neptune Theme</title>
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </head>
 <body>
--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 
 <nav id="site-navigation" class="main-navigation">
     <div class="close_open_btn">
-        <div class="close_btn">
+        <div class="close_open btn_close active">
             <ion-icon name="close-outline"></ion-icon>
         </div>
-        <div class="open_btn">
+        <div class="close_open btn_open">
             <ion-icon name="menu-outline"></ion-icon>
         </div>
         <p class="menu_text">
@@ -13,21 +13,23 @@
         </p>
     </div>
     <div class="menu_container">
-    <?php
+        <div class="menu_1">
+        <?php
     wp_nav_menu(array(
         'theme_location' => 'primary-menu',
         'menu_id' => 'primary-menu',
         'container' => false, // Remove the menu container
     ));
     ?>
-    </div>
-    <div class="menu_container">
-    <?php
-wp_nav_menu(array(
-    'theme_location' => 'second_main_menu',
-    'menu_id' => 'second_main_menu',
-));
+        </div>
+        <div class="menu_2">
+        <?php
+    wp_nav_menu(array(
+        'theme_location' => 'second_main_menu',
+        'menu_id' => 'second_main_menu',
+    ));
     ?>
+        </div>
     </div>
 </nav>
 
